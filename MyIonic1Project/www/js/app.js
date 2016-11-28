44c02afdee19db9cb630c335b3743e2eb4028052
Xia Zhe <xiazhe@outlook.com>
2016-11-28 17:37:10 +0800

    API demo

--- MyIonic1Project/www/js/app.js
+++ MyIonic1Project/www/js/app.js
@@ -7,86 +7,112 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function ($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
+    // setup an abstract state for the tabs directive
+      .state('tab', {
+          url: '/tab',
+          abstract: true,
+          templateUrl: 'templates/tabs.html'
+      })
 
-  // Each tab has its own nav history stack:
+    // Each tab has its own nav history stack:
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+    .state('tab.dash', {
+        url: '/dash',
+        views: {
+            'tab-dash': {
+                templateUrl: 'templates/tab-dash.html',
+                controller: 'DashCtrl'
+            }
         }
-      }
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+    .state('search', {
+        url: '/search',
+        templateUrl: 'templates/dash/search.html',
+        controller: 'SearchCtrl'
+    })
+   .state('search-detail', {
+        url: '/search-detail',
+        templateUrl: 'templates/dash/search-detail.html',
+        controller: 'SearchDetailCtrl'
+    })
+    .state('tab.chats', {
+        url: '/chats',
+        views: {
+            'tab-chats': {
+                templateUrl: 'templates/tab-chats.html',
+                controller: 'ChatsCtrl'
+            }
         }
-      }
     })
+      .state('tab.chat-detail', {
+          url: '/chats/:chatId',
+          views: {
+              'tab-chats': {
+                  templateUrl: 'templates/chat-detail.html',
+                  controller: 'ChatDetailCtrl'
+              }
+          }
+      })
+    .state('tab.cart', {
+        url: '/cart',
+        views: {
+            'tab-cart': {
+                templateUrl: 'templates/tab-cart.html',
+                controller: 'CartCtrl'
+            }
+        }
+    })
+    .state('tab.account', {
+        url: '/account',
+        views: {
+            'tab-account': {
+                templateUrl: 'templates/tab-account.html',
+                controller: 'AccountCtrl'
+            }
+        }
+    });
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
 
 })
-.config(['$ionicConfigProvider', function($ionicConfigProvider) {
+.config(['$ionicConfigProvider', function ($ionicConfigProvider) {
     //Place ionic tabs at the botoom of the screen
     //http://ionicframework.com/docs/api/provider/%24ionicConfigProvider/
     //http://stackoverflow.com/questions/27874855/how-to-place-ionic-tabs-at-the-bottom-of-the-screen
-    $ionicConfigProvider.tabs.position('bottom'); // other values: top
+    //https://github.com/driftyco/ionic/issues/2749
+    $ionicConfigProvider.tabs.position('bottom'); // Places them at the bottom for all OS
+    $ionicConfigProvider.tabs.style("standard"); // Makes them all look the same across all OS
+    $ionicConfigProvider.scrolling.jsScrolling(true);
+
+    $ionicConfigProvider.views.transition('platform');
+    $ionicConfigProvider.navBar.transition('platform');
+
+
+    $ionicConfigProvider.views.maxCache(10);
 
 }]);
--- MyIonic1Project/www/js/controllers.js
+++ MyIonic1Project/www/js/controllers.js
@@ -1,28 +1,79 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chats = Chats.all();
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('SearchCtrl', function ($scope, $stateParams, Chats, $ionicHistory, $window) {
+    $scope.goBack = function () {
+        $window.history.go(-1);
+        //$ionicHistory.goBack();
+    };
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('SearchDetailCtrl', function ($scope, $stateParams, Chats, $ionicHistory, $window) {
+    $scope.goBack = function () {
+        $window.history.go(-1);
+        //$ionicHistory.goBack();
+    };
+})
+
+.controller('ChatsCtrl', function ($scope, Chats) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+
+    $scope.chats = Chats.all();
+    $scope.remove = function (chat) {
+        Chats.remove(chat);
+    };
+})
+
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+    $scope.chat = Chats.get($stateParams.chatId);
+})
+.controller('CartCtrl', function ($scope) {
+    $scope.settings = {
+        enableFriends: true
+    };
+})
+
+.controller('AccountCtrl', function ($scope, $ionicModal) {
+    $ionicModal.fromTemplateUrl('templates/account/login.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
+    $scope.openModal = function () {
+        $scope.modal.show();
+    };
+    $scope.closeModal = function () {
+        $scope.modal.hide();
+    };
+    // Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function () {
+        $scope.modal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hidden', function () {
+        // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function () {
+        // Execute action
+    });
+
+
+    $scope.settings = {
+        enableFriends: true
+    };
+
+
+
 });
